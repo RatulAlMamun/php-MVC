9d63f24878c062db06d351ca369eb0979ea8d1f8
RatulAlMamun <ratulalmamun23@gmail.com>
2022-03-17 17:45:44 +0600

    feat: router and controller setup.

---
+++ controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\controllers;
+
+class ProductController
+{
+    public function index ()
+    {
+        echo "index Page";
+    }
+
+
+    public function create ()
+    {
+        echo "create Page";     
+    }
+
+
+    public function update ()
+    {
+        echo "update Page"; 
+    }
+
+    
+    public function delete ()
+    {
+        echo "delete Page";
+    }
+}
