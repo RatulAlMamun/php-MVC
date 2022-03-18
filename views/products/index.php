8e3554c2e28c7a6f86b340976a597523c876792f
RatulAlMamun <ratulalmamun23@gmail.com>
2022-03-18 18:40:59 +0600

    refactor: view page modify.

--- views/products/index.php
+++ views/products/index.php
@@ -1,38 +1,47 @@
-<h1>Product List</h1>
+<h1>Product CRUD</h1>
 
-<p><a href="/products/create" class="btn btn-success">Create Product</a></p>
-
-<form>
+<p>
+    <a href="/products/create" type="button" class="btn btn-sm btn-success">Add Product</a>
+</p>
+<form action="" method="get">
     <div class="input-group mb-3">
-        <input type="text" name="search" class="form-control" placeholder="Search your product here ..." value="<?php echo $search ?>">
-        <button class="btn btn-outline-secondary" type="submit" id="search">Search</button>
+      <input type="text" name="search" class="form-control" placeholder="Search" value="<?php echo $keyword ?>">
+      <div class="input-group-append">
+        <button class="btn btn-success" type="submit">Search</button>
+      </div>
     </div>
 </form>
-
-<table class="table table-striped">
+<table class="table">
     <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">image</th>
-            <th scope="col">title</th>
-            <th scope="col">price</th>
-            <th scope="col">created_at</th>
-            <th scope="col">Action</th>
-        </tr>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Image</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Create Date</th>
+        <th scope="col">Actions</th>
+    </tr>
     </thead>
     <tbody>
-        <?php foreach($products as $product) { ?>
-            <tr>
-                <th scope="row">1</th>
-                <td><?php echo $product["image"] ?></td>
-                <td><?php echo $product["title"] ?></td>
-                <td><?php echo $product["price"] ?></td>
-                <td><?php echo $product["created_at"] ?></td>
-                <td>
-                    <a href="/products/update" class="btn btn-outline-primary">Edit</a>
-                    <a href="/products/delete" class="btn btn-outline-danger">Delete</a>
-                </td>
-            </tr>
-        <?php } ?>
+    <?php foreach ($products as $i => $product) { ?>
+        <tr>
+            <th scope="row"><?php echo $i + 1 ?></th>
+            <td>
+                <?php if ($product['image']): ?>
+                    <img src="/<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>" class="product-img">
+                <?php endif; ?>
+            </td>
+            <td><?php echo $product['title'] ?></td>
+            <td><?php echo $product['price'] ?></td>
+            <td><?php echo $product['create_date'] ?></td>
+            <td>
+                <a href="/products/update?id=<?php echo $product['id'] ?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                <form method="post" action="/products/delete" style="display: inline-block">
+                    <input  type="hidden" name="id" value="<?php echo $product['id'] ?>"/>
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+    <?php } ?>
     </tbody>
 </table>
--- views/products/update.php
+++ views/products/update.php
@@ -1,1 +1,3 @@
-<h1>Update Product.</h1>
+<h1>Update product</h1>
+
+<?php include "_form.php"; ?>
